6daaf7e3a1198c0742ea967ceb2a0d0b2d2327c1
Avanish Singh <107432071+avanishsinghav@users.noreply.github.com>
2025-02-15 22:41:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let timing = document.getElementById("timer");
+let date = new Date();
+let formattedDate = date.toLocaleString("en-US", { hour12: true });
+timing.innerHTML = formattedDate;
